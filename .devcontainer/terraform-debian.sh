b593860fe88d2ec09ee8a26e28e9c687e9ee8975
sergiyt <github@mail.the38.dev>
2025-06-03 11:29:06 -0600

    final fix for tf and tflint

--- .devcontainer/terraform-debian.sh
+++ .devcontainer/terraform-debian.sh
@@ -56,15 +56,13 @@ fi
 TF_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_${TF_ARCH}.zip"
 TF_SHA256_URL="https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_SHA256SUMS"
 mkdir -p /tmp/tf-downloads && cd /tmp/tf-downloads
-curl -fLsS -o terraform.zip "$TF_URL"
+curl -fLsS -o terraform_${TERRAFORM_VERSION}_linux_${TF_ARCH}.zip "$TF_URL"
 curl -fLsS -o SHA256SUMS "$TF_SHA256_URL"
 if command -v sha256sum >/dev/null 2>&1; then
     grep "terraform_${TERRAFORM_VERSION}_linux_${TF_ARCH}.zip" SHA256SUMS | sha256sum -c -
 fi
-unzip terraform.zip
+unzip terraform_${TERRAFORM_VERSION}_linux_${TF_ARCH}.zip
 mv -f terraform /usr/local/bin/
-cd -
-rm -rf /tmp/tf-downloads
 
 # Download and install tflint (no official SHA256, so just download)
 if [ "${TFLINT_VERSION}" != "none" ]; then
@@ -74,6 +72,7 @@ if [ "${TFLINT_VERSION}" != "none" ]; then
     mv -f tflint /usr/local/bin/
 fi
 
-rm -rf /tmp/tf-downloads
 cd -
+rm -rf /tmp/tf-downloads
+
 echo "Done!"
